e072c852e79d4c28fe90bdc922dbb43a1e1bd7b2
jesse-orestes <jesseorestespereira1@gmail.com>
2021-03-23 21:32:25 -0300

    reset version 0.1.0

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ app.listen(port, () => {
     console.log(`Connection has established successfully in port: ${port}`);
 })
 // teste branch 002 correção
+// teste branch 003 nova versão tag
+//correção na release
